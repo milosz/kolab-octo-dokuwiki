6204fc5ff44bca08137ccb741d000c6d020ce55c
Milosz Galazka <milosz@sleeplessbeastie.eu>
2013-08-26 17:00:24 +0000

    Modified configuration file

---
+++ roundcube_plugin/dokuwiki/config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+// Dokuwiki URL
+$rcmail_config['dokuwiki_url'] = '/dokuwiki/doku.php';
+
+// authentication exchange secret
+$rcmail_config['dokuwiki_secret'] = '<shared-secret-string>';
+
